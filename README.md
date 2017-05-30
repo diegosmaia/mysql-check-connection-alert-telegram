db871c517ec09b085406507f1e7845ef6779a4ae
Diego Maia <diegosantosmaia@gmail.com>
2017-05-30 11:38:44 -0300

    Ver 1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mysql-check-connection-alert-telegram
+# Telegram Mysql Check Connection 
 Enviar msg de alerta pelo telegram se não for possivel conectar com o servidor Mysql
+
+
+Copiar o script para uma pasta, pode ser /usr/bin
+Modificar o crontab para executar o script a cada XX minutos para verficiar em caso de falha de conexão com o Banco de Dados gerar alerta pelo Telegram.
+
+
---
+++ telegram-mysql-check.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+##########################################################################
+# Linux envio de alerta de perda de conexão com Mysql pelo Telegram
+# Author: Diego Maia - diegosmaia@yahoo.com.br Telegram - @diegosmaia
+##########################################################################
+
+CURL="/usr/bin/curl"
+
+SERVIDOR=$(hostname)
+
+# Mysql user e password para conexao
+dbuser="root"
+dbpass="oldpassword"
+dbserver="192.168.0.20"
+
+# O ID DO SEU USUARIO NO TELEGRAM
+USER='150000000'
+
+############################################
+# O Bot-Token o codigo enviado pelo BotFather
+############################################
+
+BOT_TOKEN='161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58'
+
+############################################
+# Envio Mensagem de Texto do Alerta
+############################################
+
+######################################################################################
+# http://linuxtitbits.blogspot.com.br/2011/01/checking-mysql-connection-status.html
+######################################################################################
+
+dbaccess="denied"
+until [[ $dbaccess = "success" ]]; do
+	echo "Checking MySQL connection..."
+	mysql --host="${dbserver}" --user="${dbuser}" --password="${dbpass}" -e exit 2>/dev/null
+	dbstatus=`echo $?`
+	if [ $dbstatus -ne 0 ]; then
+		${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -s -X GET "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${USER}&text=Banco de dados Mysql no servidor $SERVIDOR esta Down - Data:  $(date '+%d/%m/%Y-%H:%M:%S')"  > /dev/null  
+	else
+		dbaccess="success"
+		echo "Success!"
+	fi
+done
+
+exit 0
+
+
